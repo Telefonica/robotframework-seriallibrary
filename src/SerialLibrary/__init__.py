6825990436ade02405eeba2315c2898cd7ba75c3
Jordi Puig Bou <74532516+jordipuigbou@users.noreply.github.com>
2021-11-03 10:44:23 +0100

    Add cast unicode string

--- src/SerialLibrary/__init__.py
+++ src/SerialLibrary/__init__.py
@@ -173,6 +173,8 @@ class SerialLibrary:
 
         If encoding is not specified, instance's default encoding will be used.
         """
+        if not isinstance(ustring, unicode_):
+            ustring = unic(ustring)
         return ustring.encode(encoding or self._encoding, encoding_mode)
 
     def _decode(self, bstring, encoding=None, encoding_mode='replace'):
